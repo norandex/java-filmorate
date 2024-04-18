70f4dec5808b36d584af29a196ee66aeafc42437
norandex <norilsk.andrei@yandex.ru>
2024-04-18 18:28:42 +0300

    1st attempt

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,39 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private int id;
+    @Email
+    @NotNull
+    private String email;
+    @NotBlank
+    @NotNull
+    private String login;
+    private String name;
+    @Past
+    @NotNull
+    private LocalDate birthday;
+
+    public User(int id, String email, String login, LocalDate birthday) {
+        this.id = id;
+        this.email = email;
+        this.login = login;
+        this.birthday = birthday;
+        this.name = login;
+    }
+
+    public String getName() {
+        if (this.name.isBlank()) {
+            return this.getLogin();
+        }
+        return this.name;
+    }
+}
---
+++ src/main/java/test/UserStorageTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.InvalidUserUpdateException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.services.UserStorage;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class UserStorageTest {
+
+    UserStorage us;
+
+    @BeforeEach
+    public void createFilmStorage() {
+        this.us = new UserStorage();
+    }
+
+    @Test
+    public void testInvalidFilmUpdateException() {
+        User user = new User(1, "Name", "login", LocalDate.of(2000, Month.DECEMBER, 28));
+        us.createUser(user);
+        User userUnknownId = new User(22, "Name", "login", LocalDate.of(2000, Month.DECEMBER, 28));
+        Assertions.assertThrows(InvalidUserUpdateException.class, () -> us.updateUser(userUnknownId));
+    }
+}
