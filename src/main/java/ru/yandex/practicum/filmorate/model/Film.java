4068cfede27de9ad72b24e18d828f3fdaae7dbdb
norandex <129898970+norandex@users.noreply.github.com>
2024-04-18 17:35:17 +0300

    Revert "#1 Sprint 9"

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,26 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
-import java.time.LocalDate;
-
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Film.
  */
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
 public class Film {
-    private int id;
-    @NotBlank
-    private String name;
-    @Size(max = 200)
-    private String description;
-    @Positive
-    private long duration;
-    private LocalDate releaseDate;
 }
