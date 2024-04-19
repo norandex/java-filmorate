2976fc1256beaba0c925cd72c03ed933118b936c
norandex <norilsk.andrei@yandex.ru>
2024-04-19 20:09:39 +0300

    3rd attempt 1st iter

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
