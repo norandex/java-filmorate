166105aef1206de354281f6b45c5c3863a2d020b
norandex <norilsk.andrei@yandex.ru>
2024-04-27 12:50:22 +0300

    sprint 10, 2nd attempt

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -6,11 +6,18 @@ import java.util.List;
 
 public interface UserService {
     User create(User user);
+
     User getUserById(Long id);
+
     List<User> getCommonFriends(Long id, Long otherId);
+
     List<User> readAllUsers();
+
     User updateUser(User user);
+
     User makeFriends(Long userId, Long friendId);
+
     User deleteFriends(Long userId, Long friendId);
+
     List<User> getFriendsOfUser(Long id);
 }
