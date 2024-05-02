e19c0905c401aa874b94a5d1ca33e910e0e7c3a7
norandex <norilsk.andrei@yandex.ru>
2024-05-02 08:10:13 +0300

    sprint 11, 1.1 attempt

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO mpa_rating (name)
+VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
+
+INSERT INTO genres (name)
+VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
+
+INSERT INTO statuses (status)
+VALUES ('requested'), ('accepted');
